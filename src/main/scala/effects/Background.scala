975e729317fd981d96e077cc3711735198a8b05a
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-12-25 17:51:20 +0400

    GH-000 Finish Chapter 04

--- src/main/scala/effects/Background.scala
+++ src/main/scala/effects/Background.scala
@@ -1,6 +1,10 @@
 package ge.zgharbi.pfps
 package effects
 
+import cats.effect._
+import cats.effect.std._
+import cats.syntax.all._
+
 import scala.concurrent.duration.FiniteDuration
 
 trait Background[F[_]] {
@@ -9,4 +13,13 @@ trait Background[F[_]] {
 
 object Background {
   def apply[F[_]: Background]: Background[F] = implicitly
+
+  implicit def bgInstance[F[_]](implicit
+    S: Supervisor[F],
+    T: Temporal[F],
+  ): Background[F] =
+    new Background[F] {
+      override def schedule[A](fa: F[A], duration: FiniteDuration): F[Unit] =
+        S.supervise(T.sleep(duration) *> fa).void
+    }
 }
